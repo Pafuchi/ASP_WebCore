64ebadc21d540b3a20ab8287eebbbf9430a93ca5
Hoang Vu Dinh <dinh@SPSEJECNANET.CZ>
2019-05-07 09:04:48 +0200

    Web core avarage calculation

---
+++ AvarageCalculator/WebApplication2/Pages/Calculator/Avarage.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Pages
+{
+ public class AvarageModel : PageModel
+ {
+  public byte PocetCisel { get; set; }
+  [BindProperty]
+  public Dictionary<Byte, Decimal> Values { get; set; }
+  public Decimal AvarageNum { get; set; }
+
+  public void OnGet(Byte KolikCisel)
+  {
+   this.PocetCisel = KolikCisel;
+  }
+
+  public void OnPost()
+  {
+   AvarageNum = Values.Average(k => k.Value);
+  }
+ }
+}
---
+++ SumCalculator/WebApplication1/Pages/Calculator/Addition.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages.Calculator
+{
+ public class AdditionModel : PageModel
+ {
+  [BindProperty]
+  public decimal VarA { get; set; }
+  [BindProperty]
+  public decimal VarB { get; set; }
+  [BindProperty]
+  public decimal Result { get; set; }
+
+  public void OnGet()
+  {
+
+  }
+
+  public void OnPost()
+  {
+   this.Result = this.VarA + this.VarB;
+  }
+
+ }
+}

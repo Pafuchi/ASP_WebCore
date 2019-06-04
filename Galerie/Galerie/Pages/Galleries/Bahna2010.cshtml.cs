fb27fc68bb7e241bd129acaa12ad59958b52a5a0
Hoang Vu Dinh <dinh@SPSEJECNANET.CZ>
2019-06-04 09:10:48 +0200

    galerie

---
+++ Galerie/Galerie/Pages/Galleries/Bahna2010.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Galerie.Pages.Galleries
+{
+    public class Bahna2010Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
